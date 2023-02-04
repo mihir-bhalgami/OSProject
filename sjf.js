a44c90d05acfb93cb6a2123002ce7c093b55a16d
viraj812 <124186730+viraj812@users.noreply.github.com>
2023-02-04 18:54:52 +0530

    Add files via upload

---
+++ sjf.js
@@ -0,0 +1,8 @@
+function calcProcess(){
+    var num_of_processes = document.getElementById("num_of_processes").textContent;
+    return num_of_processes;
+}
+
+function calcBurstTime(){
+    var 
+}
