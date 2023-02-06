612399cb29e7aa46ec65e3c9ff0750523e8a52ae
viraj812 <124186730+viraj812@users.noreply.github.com>
2023-02-06 15:53:23 +0530

    Update sjf.js

--- sjf.js
+++ sjf.js
@@ -1,8 +1,18 @@
-function calcProcess(){
-    var num_of_processes = document.getElementById("num_of_processes").textContent;
-    return num_of_processes;
+var num_of_processes = prompt("Enter number of processes");
+
+function showProcessNumber(){
+    var p = document.getElementById("processNumber");
+    p.innerHTML = "Process " + num + ": " ;  
 }
 
-function calcBurstTime(){
-    var 
+function getData(){
+    var num_of_processes = document.getElementById("num_of_processes").textContent;
+    
+    var arrival_time = document.getElementById("arrival_time").textContent;
+    
+    var burst_time = document.getElementById("burst_time").textContent;
+
+    return num_of_processes, arrival_time, burst_time;
 }
+
+
